ae1d80a5e68603e6297bae30ba531d0bc0a0f791
AissatouCISSE <aissatoucisse351@gmail.com>
2021-01-23 18:44:40 +0100

    TA_Android

---
+++ app/src/main/java/simplon/sn/MainActivity2.java
@@ -0,0 +1,20 @@
+package simplon.sn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+    }
+}
